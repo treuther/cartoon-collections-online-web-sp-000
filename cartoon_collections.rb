df1f632159b4718769ec7811a47497f1fff7e166
Tracy Reuther <tracyreuther@gmail.com>
2019-06-02 11:33:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
 def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  new_dwarves = []
+  dwarves.each_with_index { |item, index}
+    new_dwarves[item] = index
 end
 
 def summon_captain_planet# code an argument here
