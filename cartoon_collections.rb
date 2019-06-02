307cc531ad30955edd4141c6323c8868d709c2b9
Tracy Reuther <tracyreuther@gmail.com>
2019-06-02 12:27:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,6 +18,7 @@ def summon_captain_planet(array)# code an argument here
   planeteer_calls
 end
 
+require 'pry'
 def long_planeteer_calls(array)# code an argument here
   # Your code here
   array.any? do |word|
@@ -25,6 +26,7 @@ def long_planeteer_calls(array)# code an argument here
   end
 end
 
+binding.pry
 
 def find_the_cheese# code an argument here
   # the array below is here to help
