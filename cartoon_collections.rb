e276caba0d404f86b4cbb2d8e9026fbb51272d7a
Tracy Reuther <tracyreuther@gmail.com>
2019-06-02 11:48:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,11 +9,11 @@ def roll_call_dwarves(dwarves)# code an argument here
 end
 
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(veggies)# code an argument here
   # Your code here
 end
 
-def long_planeteer_calls(veggies)# code an argument here
+def long_planeteer_calls# code an argument here
   # Your code here
 end
 
