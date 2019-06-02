f5d3c91206ecc2074450420c53024a1c28af93af
Tracy Reuther <tracyreuther@gmail.com>
2019-06-02 11:47:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(dwarves)# code an argument here
   new_dwarves = []
   dwarves.each_with_index do |item, index|
     index += 1
-    puts "#{index} #{item}"
+    puts "#{index}. #{item}"
   end
   new_dwarves
 end
