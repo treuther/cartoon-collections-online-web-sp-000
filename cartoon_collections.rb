78111f9ba3193062d65c93c9597808a39139ac13
Tracy Reuther <tracyreuther@gmail.com>
2019-06-02 12:28:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,15 +18,12 @@ def summon_captain_planet(array)# code an argument here
   planeteer_calls
 end
 
-require 'pry'
 def long_planeteer_calls(array)# code an argument here
   # Your code here
   array.any? do |word|
     word.length > 4
   end
 end
-
-binding.pry
 
 def find_the_cheese# code an argument here
   # the array below is here to help
