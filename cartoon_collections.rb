4c054b27c490778fc050b0c197aedc6f38e4ce44
Tracy Reuther <tracyreuther@gmail.com>
2019-06-02 11:54:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,6 +11,7 @@ end
 
 def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+  planeteer_calls.map! {|calls| calls.capitalize puts "#{calls}!"}
 end
 
 def long_planeteer_calls# code an argument here
