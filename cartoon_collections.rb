9e80f08986b1790843a645bd5b1c4576af4b9cf7
Tracy Reuther <tracyreuther@gmail.com>
2019-06-02 12:25:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,9 +20,11 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
-  array.any? {|i| i > 4}
+  array.any? do |word|
+    word.length > 4
+  end
 end
-long_planeteer_calls(array)
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
